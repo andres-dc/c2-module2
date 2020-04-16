795d3f69283a6f92ffa0ec612dcd846a1d042fbc
Andrés Del Carpio <andresdc@andresdc.net>
2020-04-16 15:55:45 -0500

    Initial Commit of Contact Book

---
+++ week-1/day-3/contact-book.rb
@@ -0,0 +1,62 @@
+# frozen_string_literal: true
+
+contacts = {
+  Andres: 22,
+  Gretzky: 99,
+  Lemieux: 66
+}
+
+puts 'What would you like to do?'
+puts "-- Type 'add' or 'a' to add a contact."
+puts "-- Type 'update' or 'u' to update a contact."
+puts "-- Type 'display' or 'd' to display all contacts."
+puts "-- Type 'delete' or 'del' to delete a contact."
+
+choice = gets.chomp.downcase
+
+case choice
+when 'add' && 'a'
+  puts 'Contact name: '
+  name = gets.chomp
+
+  if contacts[name.to_sym].nil?
+    puts 'Number: '
+    number = gets.chomp
+    contacts[name.to_sym] = number.to_i
+    puts "Name: #{name} - Number: #{number}"
+  else
+    puts "That contact already exists with number #{contacts[name.to_sym]}"
+  end
+
+when 'update' && 'u'
+  puts 'Contact name: '
+  name = gets.chomp
+
+  if contacts[name.to_sym].nil?
+    puts 'Contact not found.'
+  else
+    puts 'New number: '
+    number = gets.chomp
+    contacts[name.to_sym] = number.to_i
+    puts "Name: #{name} - Number: #{number}"
+  end
+
+when 'display' && 'd'
+  contacts.each do |name, number|
+    puts "Name: #{name} - Number: #{number}"
+  end
+
+when 'delete' && 'del'
+  puts 'Contact name: '
+  name = gets.chomp
+
+  if contacts[name.to_sym].nil?
+    puts 'Contact not found.'
+  else
+    contacts.delete(name.to_sym)
+
+    contacts.each do |name, number|
+      puts "Name: #{name} - Number: #{number}"
+    end
+  end
+end

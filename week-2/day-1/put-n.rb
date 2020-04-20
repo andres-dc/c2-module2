85404df3774bc862f91eeb1fd2e2b356f257f3ff
Andrés Del Carpio <andresdc@andresdc.net>
2020-04-20 15:37:53 -0500

    Fixed

--- week-2/day-1/put-n.rb
+++ week-2/day-1/put-n.rb
@@ -1,10 +1,10 @@
 def put_n(str, t)
-  raise ArgumentError.new("Number is negative") if !(t >= 1)
+  raise ArgumentError.new("Number is negative") if t <= 0
   raise ArgumentError.new("Number is not an integer") if !(t.is_a? Integer)
     
   t.times { puts str }
 end
 
 put_n("This will be printed 5 times!", 5)
-put_n("This will be printed 5 times!", -2)
 put_n("This will be printed 5 times!", 3.5)
+put_n("This will be printed 5 times!", -2)

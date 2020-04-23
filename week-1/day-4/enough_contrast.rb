3b6f28b7ce5c494bb3b58cfb7eb63d3d742a6e1c
Andrés Del Carpio <andresdc@andresdc.net>
2020-04-22 20:07:11 -0500

    Solution for Enough Contrast?

---
+++ week-1/day-4/enough_contrast.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+class Color
+  attr_reader :r, :g, :b
+  def initialize(red, green, blue)
+    @r = red
+    @g = green
+    @b = blue
+  end
+
+  def brightness_index
+    (299 * @r + 587 * @g + 114 * @b) / 1000
+  end
+
+  def brightness_difference(second_clr)
+    brightness_index - second_clr.brightness_index
+  end
+
+  def hue_difference(second_clr)
+    ((r - second_clr.r) + (g - second_clr.g) + (b - second_clr.b)).abs
+  end
+
+  def enough_contrast?(second_clr)
+    brightness_difference(second_clr) > 125 && hue_difference(second_clr) > 50
+  end
+end
+
+color1 = Color.new(64, 98, 41)
+color2 = Color.new(201, 14, 152)
+color3 = Color.new(1, 200, 90)
+color4 = Color.new(113, 213, 85)
+
+p color1.brightness_difference(color2)
+p color3.hue_difference(color1)
+p color2.enough_contrast?(color4)

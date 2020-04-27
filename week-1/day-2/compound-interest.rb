e166caab9aff9cde15b9515f5242c45675982241
Andrés Del Carpio <andresdc@andresdc.net>
2020-04-27 15:47:30 -0500

    Minor fixes

--- week-1/day-2/compound-interest.rb
+++ week-1/day-2/compound-interest.rb
@@ -7,11 +7,13 @@ money = gets.chomp.to_f
 puts 'Enter the interest rate: '
 interest = gets.chomp.to_f
 
-def interest (months, money, interest)
+def interest(months, money, interest)
   i = 1
-  while (i <= months)
-    money = money + (money * interest / 100)
+  while i <= months
+    money += (money * interest / 100)
     i += 1
   end
   money.round(2)
 end
+
+p interest(months, money, interest)

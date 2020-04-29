4aaa5fe72ffcb46c25ebca30b4c8ea76287588b7
Andrés Del Carpio <andresdc@andresdc.net>
2020-04-28 23:34:13 -0500

    Minor fix

--- week-1/day-1/absolute-sum.rb
+++ week-1/day-1/absolute-sum.rb
@@ -1,14 +1,4 @@
-def absolute_sum1 (a, b)
-  sum = a + b
-
-  if (a + b < 0)
-    sum * -1
-  else
-    sum
-  end
-end
-
-def absolute_sum2 (a, b)
+def absolute_sum(a, b)
   sum = a + b
 
   a + b < 0 ? sum * -1 : sum

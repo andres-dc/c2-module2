e4125fde98f2ae7108950de420101f7640f468a2
Andrés Del Carpio <andresdc@andresdc.net>
2020-04-28 23:43:26 -0500

    Minor refactoring and fixed file name

--- week-1/day-2/who_is_online.rb
+++ week-1/day-2/who_is_online.rb
@@ -2,14 +2,14 @@ def who_is_online(friends)
   out = { "online" => [], "offline" => [], "away" => [] }
 
   friends.each do |x|
-    if (x["status"] == "online" && x["last_activity"] <= 10)
+    if x["status"] == "online" && x["last_activity"] <= 10
       out["online"] << x["username"]
-    elsif (x["status"] == "offline")
+    elsif x["status"] == "offline"
       out["offline"] << x["username"]
     else
       out["away"] << x["username"]
     end
   end
-  
+
   out.select! { |_k, v| v != [] }
 end
